c0c7659e4cda7fcc711867c3f0c2467f99a5e584
VuPhong <vuphong0x@gmail.com>
2020-11-10 21:46:23 +0700

    Ahihi

--- app/src/androidTest/java/com/teamone/fastandsafe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/teamone/fastandsafe/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.teamone.sama", appContext.getPackageName());
+        assertEquals("com.teamone.fastandsafe", appContext.getPackageName());
     }
 }
