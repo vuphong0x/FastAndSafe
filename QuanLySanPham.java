fde287708fc47438b3fefb6394ec7a63daf85779
ductntn <74250459+ductntn@users.noreply.github.com>
2020-11-11 13:18:24 +0700

    Add files via upload

---
+++ QuanLySanPham.java
@@ -0,0 +1,16 @@
+package com.teamone.fastandsafe.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.teamone.fastandsafe.R;
+
+public class QuanLySanPham extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quan_ly_san_pham);
+    }
+}
